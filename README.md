b34c211ad367ce3bf03e73cecca010cf51f29cc6
kainaga shu <k221t037@st.kyoto-seika.ac.jp>
2023-08-10 15:14:32 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Seika-2q-cc
 # Seika-2q-kadai
+# Seika-2q-kadai
--- dist/app.js
+++ dist/app.js
@@ -1,21 +1,75 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!********************!*\
-  !*** ./src/app.js ***!
-  \********************/
-document.addEventListener('DOMContentLoaded', function() {
-	var animateImages = document.querySelectorAll('.animate-image');
-    
-	animateImages.forEach(function(image) {
-	  image.addEventListener('click', function() {
-		anime({
-			targets: '.image-container img',
-			translateX: 250
-		    });
-	  });
-	});
-    });
-    
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBLE9BQU87QUFDUCxJQUFJO0FBQ0osRUFBRTtBQUNGLEtBQUs7QUFDTCxJIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vaG9tZXBhZ2UvLi9zcmMvYXBwLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImRvY3VtZW50LmFkZEV2ZW50TGlzdGVuZXIoJ0RPTUNvbnRlbnRMb2FkZWQnLCBmdW5jdGlvbigpIHtcblx0dmFyIGFuaW1hdGVJbWFnZXMgPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yQWxsKCcuYW5pbWF0ZS1pbWFnZScpO1xuICAgIFxuXHRhbmltYXRlSW1hZ2VzLmZvckVhY2goZnVuY3Rpb24oaW1hZ2UpIHtcblx0ICBpbWFnZS5hZGRFdmVudExpc3RlbmVyKCdjbGljaycsIGZ1bmN0aW9uKCkge1xuXHRcdGFuaW1lKHtcblx0XHRcdHRhcmdldHM6ICcuaW1hZ2UtY29udGFpbmVyIGltZycsXG5cdFx0XHR0cmFuc2xhdGVYOiAyNTBcblx0XHQgICAgfSk7XG5cdCAgfSk7XG5cdH0pO1xuICAgIH0pO1xuICAgICJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
+const addAnimationButton = document.getElementById('addAnimationButton');
+const animationContainer = document.getElementById('animationContainer');
+const changeShapeButton = document.getElementById('changeShapeButton');
+const scatterButton = document.getElementById('scatterButton');
+const resetButton = document.getElementById('resetButton');
+
+let initialState = []; // 初期状態を記録するための配列
+
+addAnimationButton.addEventListener('click', () => {
+  const newElement = document.createElement('div');
+  newElement.classList.add('box');
+  animationContainer.appendChild(newElement);
+  
+  initialState.push({
+    width: '100px',
+    height: '100px',
+    backgroundColor: 'blue',
+    borderRadius: '0%'
+  });
+});
+
+changeShapeButton.addEventListener('click', () => {
+  const shapes = [
+    { width: '100px', height: '100px', borderRadius: '0%' },
+    { width: '150px', height: '75px', borderRadius: '50%' },
+    { width: '75px', height: '150px', borderRadius: '50%' }
+  ];
+
+  const randomShape = shapes[Math.floor(Math.random() * shapes.length)];
+
+  anime({
+    targets: '.box',
+    width: randomShape.width,
+    height: randomShape.height,
+    backgroundColor: getRandomColor(),
+    borderRadius: randomShape.borderRadius,
+    easing: 'easeInOutQuad',
+    duration: 1000
+  });
+});
+
+scatterButton.addEventListener('click', () => {
+  anime({
+    targets: '.box',
+    translateX: () => anime.random(-300, 300),
+    translateY: () => anime.random(-300, 300),
+    rotate: () => anime.random(0, 360),
+    easing: 'easeInOutQuad',
+    duration: 1000
+  });
+});
+
+resetButton.addEventListener('click', () => {
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach(box => {
+    animationContainer.removeChild(box); // 要素を削除
+  });
+
+  initialState = []; // 初期状態をリセット
+});
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initialState = [
+    { width: '100px', height: '100px', backgroundColor: 'blue', borderRadius: '0%' }
+  ];
+});
